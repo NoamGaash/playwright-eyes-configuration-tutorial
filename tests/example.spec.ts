8510a81776bed02c21855519531064e1a8cae631
Noam Gaash <noam.gaash@applitools.com>
2025-04-07 16:41:20 +0300

    example: test.use

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,5 +1,23 @@
 import { expect, test } from '@applitools/eyes-playwright/fixture';
 
+test.use({
+  eyesConfig: {
+    testName: (testInfo) => `Eyes Playwright - ${testInfo.title}`,
+    browsersInfo: [
+      {
+        name: 'chrome',
+        width: 800,
+        height: 600,
+      },
+      {
+        name: 'firefox',
+        width: 800,
+        height: 600,
+      }
+    ]
+  }
+})
+
 test('basic check', async ({ page, eyes }) => {
   await page.setContent('<h1>Hello World</h1>');
 
